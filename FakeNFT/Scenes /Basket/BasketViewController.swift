510b8e818eac58b1b005cba75c03b2c268fc78f1
alekseiypetrov <aleshapetrov03@gmail.com>
2025-12-22 19:21:27 +0300

    feat(ui): выполнена верстка navigationBar для экрана корзины

--- FakeNFT/Scenes /Basket/BasketViewController.swift
+++ FakeNFT/Scenes /Basket/BasketViewController.swift
@@ -1,3 +1,42 @@
 import UIKit
 
-final class BasketViewController: UIViewController { }
+final class BasketViewController: UIViewController {
+    
+    // MARK: - Constants
+    
+    private enum Constants {
+        static let sortingButtonImage = UIImage(resource: .nbSort)
+    }
+    
+    // MARK: - UI-elements
+    
+    private lazy var sortingButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(
+            image: Constants.sortingButtonImage,
+            style: .plain,
+            target: self,
+            action: #selector(sortingButtonPressed)
+        )
+        button.tintColor = UIColor(resource: .ypBlack)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    // MARK: - Actions
+    
+    @objc
+    private func sortingButtonPressed() { }
+    
+    // MARK: - Private Methods
+    
+    private func setupUI() {
+        view.backgroundColor = UIColor(resource: .ypWhite)
+        navigationItem.rightBarButtonItem = sortingButton
+    }
+}
