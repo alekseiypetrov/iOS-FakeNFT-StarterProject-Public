6a48e0c5c7fc5c4e5b475b3fe5101d5dba4c88de
alekseiypetrov <aleshapetrov03@gmail.com>
2025-12-23 20:16:45 +0300

    fix(core): добавлен API-ключ

--- FakeNFT/Services/Requests/RequestConstants.swift
+++ FakeNFT/Services/Requests/RequestConstants.swift
@@ -1,5 +1,4 @@
 enum RequestConstants {
     static let baseURL = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net"
-    #warning("Instert your token here")
-    static let token = ""
+    static let token = "8cd90043-9387-45ea-8ceb-cb6ee8e82849"
 }
