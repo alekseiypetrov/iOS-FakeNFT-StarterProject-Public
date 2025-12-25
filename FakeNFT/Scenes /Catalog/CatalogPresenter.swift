914e26911ee7257bac39f05d5ddb9179f430c0aa
Crocko47 <teoremaabela@yahoo.com>
2025-12-25 19:57:22 +0800

    feat: Экран каталога: Создать Presenter экрана каталога

---
+++ FakeNFT/Scenes /Catalog/CatalogPresenter.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+final class CatalogPresenter: CatalogPresenterProtocol {
+
+    // MARK: - Lifecycle
+
+    init() {
+        // пока без зависимостей
+    }
+
+    // MARK: - CatalogPresenterProtocol
+
+    func viewDidLoad() {
+        // логика загрузки
+    }
+}
---
+++ FakeNFT/Scenes /Catalog/CatalogPresenterProtocol.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+protocol CatalogPresenterProtocol: AnyObject {
+    func viewDidLoad()
+}
