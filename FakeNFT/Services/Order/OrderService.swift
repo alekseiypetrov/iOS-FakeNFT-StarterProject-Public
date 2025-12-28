7c0a13b59e7d9d7b3a9b0511a6721e4bec0df9ce
alekseiypetrov <aleshapetrov777@mail.ru>
2025-12-28 12:30:54 +0300

    fix(core): изменена логика выполнения сетевых запросов, устранения ситуаций race condition и приостановка запросов, если пользователь уйдет с экрана корзины.
    Для этого были переопределены методы viewWillAppear для подгрузки данных и viewDidDisappear для остановки загрузки данных во ViewController'е, а также определены и реализованы идентичные методы в Presenter'е. В сетевых сервисах для загрузки корзины и каждого NFT добавлены свойство сетевых задач NetworkTask и метод остановки задачи при уходе из экрана корзины. Убрано взаимодействие ViewController'а и Presenter'а через viewDidLoad (также этот метод убран внутри самого Presenter'а) и заменено в viewWillAppear и viewDidDisappear.

--- FakeNFT/Services/Order/OrderService.swift
+++ FakeNFT/Services/Order/OrderService.swift
@@ -4,18 +4,25 @@ typealias OrderCompletion = (Result<Order, Error>) -> Void
 
 protocol OrderService {
     func loadOrder(completion: @escaping OrderCompletion)
+    func stopLoading()
 }
 
 final class OrderLoader: OrderService {
     private let networkClient: NetworkClient
+    private var task: NetworkTask?
 
     init(networkClient: NetworkClient) {
         self.networkClient = networkClient
     }
     
     func loadOrder(completion: @escaping OrderCompletion) {
+        if let task { return }
+        
         let request = OrderRequest()
-        networkClient.send(request: request, type: Order.self) { result in
+        task = networkClient.send(request: request, type: Order.self) { result in
+            defer {
+                self.task = nil
+            }
             switch result {
             case .failure(let error):
                 completion(.failure(error))
@@ -23,5 +30,10 @@ final class OrderLoader: OrderService {
                 completion(.success(order))
             }
         }
+    }
+    
+    func stopLoading() {
+        task?.cancel()
+        task = nil
     }
 }
