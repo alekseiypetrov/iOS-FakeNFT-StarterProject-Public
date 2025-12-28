9ffb8b0035c1e8fcdc64ac281001c632014e6506
alekseiypetrov <aleshapetrov777@mail.ru>
2025-12-28 11:31:29 +0300

    fix(core): добавлена "обертка" для ProgressHUD

---
+++ FakeNFT/Foundation/UIProgressHUD.swift
@@ -0,0 +1,12 @@
+import UIKit
+import ProgressHUD
+
+final class UIProgressHUD {
+    static func show() {
+        ProgressHUD.show()
+    }
+    
+    static func dismiss() {
+        ProgressHUD.dismiss()
+    }
+}
