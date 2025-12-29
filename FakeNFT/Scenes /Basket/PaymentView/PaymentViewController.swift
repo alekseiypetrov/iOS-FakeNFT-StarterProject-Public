71ffe6626545a063623c601e4e39f029382bea30
alekseiypetrov <aleshapetrov777@mail.ru>
2025-12-29 16:27:53 +0300

    feat(ui): выполнены начальная верстка коллекции и конформы PaymentViewController'а на необходимые протоколы, а также создана собственная ячейка коллекции PaymentCollectionViewCell

--- FakeNFT/Scenes /Basket/PaymentView/PaymentViewController.swift
+++ FakeNFT/Scenes /Basket/PaymentView/PaymentViewController.swift
@@ -22,6 +22,18 @@ final class PaymentViewController: UIViewController {
         return button
     }()
     
+    private lazy var collectionView: UICollectionView = {
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collection.backgroundColor = self.view.backgroundColor
+        collection.allowsSelection = true
+        collection.allowsMultipleSelection = false
+        collection.isScrollEnabled = false
+        collection.delegate = self
+        collection.dataSource = self
+        collection.register(PaymentCollectionViewCell.self)
+        return collection
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -42,5 +54,48 @@ final class PaymentViewController: UIViewController {
         view.backgroundColor = UIColor(resource: .ypWhite)
         navigationItem.leftBarButtonItem = backButton
         navigationItem.title = Constants.navigationBarTitle
+        
+        NSLayoutConstraint.activate([
+            
+            // collectionView Constraints
+            
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20.0),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16.0),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16.0),
+            // TODO: - will be fixed later (поменять на topAnchor "карточки" для подтверждения и выполнения оплаты)
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - PaymentViewController + UICollectionViewDataSource
+
+extension PaymentViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+}
+
+// MARK: - PaymentViewController + UICollectionViewDelegate
+
+extension PaymentViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        <#code#>
     }
 }
