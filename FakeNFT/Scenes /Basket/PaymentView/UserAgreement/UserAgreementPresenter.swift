0cec9997f3bdda07be590977e44d29f47923b837
alekseiypetrov <aleshapetrov777@mail.ru>
2026-01-08 19:55:00 +0300

    fix(core): добавлено логирование на экране показа пользовательского соглашения

--- FakeNFT/Scenes /Basket/PaymentView/UserAgreement/UserAgreementPresenter.swift
+++ FakeNFT/Scenes /Basket/PaymentView/UserAgreement/UserAgreementPresenter.swift
@@ -35,20 +35,25 @@ extension UserAgreementPresenter: UserAgreementPresenterProtocol {
     func viewWillAppear() {
         guard let request = makeRequest()
         else {
+            print("[UserAgreementPresenter/viewWillAppear]: неверный запрос")
             viewController?.showError()
             return
         }
+        print("[UserAgreementPresenter/viewWillAppear]: запрос сформирован. Начало загрузки страницы")
         updateProgress(toValue: 0.0)
         viewController?.startLoadingPage(withRequest: request)
     }
     
     func viewDidDisappear() {
+        print("[UserAgreementPresenter/viewDidDisppear]: прекращение загрузки страницы")
         viewController?.stopLoadingPage()
     }
     
     func updateProgress(toValue value: Double) {
+        print("[UserAgreementPresenter/updateProgress]: Страница загружена на \(round(100 * value))%")
         viewController?.setNewProgressValue(CGFloat(value))
         if abs(value - 1.0) <= 0.001 {
+            print("[UserAgreementPresenter/updateProgress]: Страница загружена")
             viewController?.hideProgress()
         }
     }
