1bf8dc423f52623a4bdf623b429e59d8f28e7eb7
alekseiypetrov <aleshapetrov777@mail.ru>
2026-01-09 01:33:50 +0300

    fix(core): в модели валюты добавлен id для реализации будущих сетевых запросов

--- FakeNFT/Models/Basket/Currency.swift
+++ FakeNFT/Models/Basket/Currency.swift
@@ -1,9 +1,11 @@
 struct Currency: Decodable {
+    let id: String
     let title: String
     let name: String
     let imageName: String
     
     private enum CodingKeys: String, CodingKey {
+        case id
         case title
         case name
         case imageName = "image"
